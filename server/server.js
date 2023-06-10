939c2b3e8b6c5ddd5cb2b5ee1a09b3a3ac66f9e6
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-06-10 15:10:46 -0400

    Added DB connection
    Added a file to connect and export a pool and also set up env variables

--- server/server.js
+++ server/server.js
@@ -2,6 +2,16 @@ const PORT = process.env.PORT ?? 8000;
 const express = require('express');
 const app = express();
 
+// //get all todos
+// app.get('/todos', async (req, res)=>{
+//     try{
+
+//     }
+//     catch(err){
+//         console.error(err);
+//     }
+// });
+
 app.listen(PORT, () => {
     console.log(`server running on PORT ${PORT}`);
 });
