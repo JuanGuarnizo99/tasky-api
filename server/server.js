60e5d9a1e51788c3ca98d23a7806910246841be6
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-06-10 15:41:02 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require('express');
 const app = express();
+const pool = require('./db');
 
-// //get all todos
-// app.get('/todos', async (req, res)=>{
-//     try{
-
-//     }
-//     catch(err){
-//         console.error(err);
-//     }
-// });
+//get all todos
+app.get('/todos', async (req, res)=>{
+    try{
+        const todos = await pool.query('SELECT * FROM todos');
+        res.json(todos.rows);
+    }
+    catch(err){
+        console.error(err);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`server running on PORT ${PORT}`);
