e0a37e36c0a4388f9cc0a0431c91002799707b0b
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-06-15 10:07:20 -0400

    Update db.js
    DB is connected now

--- server/db.js
+++ server/db.js
@@ -2,24 +2,24 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
+    user: process.env.DBUSERNAME,
+    password: process.env.DBPASSWORD,
+    host: process.env.DBHOST,
     port: process.env.DBPORT,
     database: 'tasky'
 });
 
-const test_connection = async () => {
-    try{
-        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', ['juansito@gmail.com']);
-        console.log(todos.rows);
-    }
-    catch(err){
-        console.error(err);
-    }
-};
+// const test_connection = async () => {
+//     try{
+//         const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', ['juansito@gmail.com']);
+//         console.log(todos.rows);
+//     }
+//     catch(err){
+//         console.error(err);
+//     }
+// };
 
-test_connection();
+// test_connection();
 
 
 module.exports = pool;
