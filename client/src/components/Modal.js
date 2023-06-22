bbd7e5a5af35ffac7d3f01e5f3d55f7d8900f161
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-06-22 12:17:26 -0400

    Added POST todos endpoint

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,37 +1,51 @@
-import {useState} from "react";
+import { useState } from "react";
 
-
-function Modal({mode, setShowModal, task}) {
-  const editMode = mode === "edit"? true : false;
+function Modal({ mode, setShowModal, task }) {
+  const editMode = mode === "edit" ? true : false;
 
   // data object represents a task
   const [data, setData] = useState({
-    user_email: editMode? task.user_email : null,
-    title: editMode? task.title : null,
-    progress: editMode? task.progress : 50,
-    date: editMode? "" : new Date()
+    user_email: editMode ? task.user_email : "bob@test.com",
+    title: editMode ? task.title : "test title",
+    progress: editMode ? task.progress : 50,
+    date: editMode ? "" : new Date(),
   });
+
+  // Posts the data object to the backend
+  const postData = async (e) => {
+    e.preventDefault();
+
+    await fetch(`http://localhost:8000/todos`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
 
   // changes the data object when typing on the input components
   const handleChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
-    setData(data => ({
+    setData((data) => ({
       ...data,
-      [name]: value
+      [name]: value,
     }));
 
     console.log(data);
-  }
+  };
 
   return (
-    <div className= "overlay">
+    <div className="overlay">
       <div className="modal">
-        <div className= "form-title-container">
+        <div className="form-title-container">
           <h3>Let's {mode} your task</h3>
           <button onClick={() => setShowModal(false)}>X</button>
         </div>
-        <form>
+        <form onSubmit={editMode ? "" : postData}>
           <input
             required
             maxLength="30"
@@ -39,9 +53,9 @@ function Modal({mode, setShowModal, task}) {
             name="title"
             value={data.title}
             onChange={handleChange}
-          /> 
-          <br/>
-          <label for="range">Drag to select your current progress</label>
+          />
+          <br />
+          <label>Drag to select your current progress</label>
           <input
             required
             id="range"
@@ -51,15 +65,14 @@ function Modal({mode, setShowModal, task}) {
             name="progress"
             value={data.progress}
             onChange={handleChange}
-          /> 
-          <br/>
-          
-          <input className={mode} type="submit"/>
+          />
+          <br />
 
+          <input className={mode} type="submit" />
         </form>
       </div>
     </div>
   );
 }
-  
+
 export default Modal;
--- server/server.js
+++ server/server.js
@@ -3,14 +3,15 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const pool = require('./db');
+const {v4: uuidv4} = require('uuid');
 
 
 app.use(cors());
+app.use(express.json());
 
 // DEMO ROUTE get all todos
 app.get('/todos/:userEmail', async (req, res)=>{
 
-    console.log(req);
     const {userEmail} = req.params;
 
     try{
@@ -20,6 +21,24 @@ app.get('/todos/:userEmail', async (req, res)=>{
     }
     catch(err){
         console.error(err);
+    }
+});
+
+// Create a new todo
+app.post('/todos', async (req, res)=>{
+    const {user_email, title, progress, date} = req.body;
+    console.log(user_email, title, progress, date);
+    //call uuidv4 to get a unique id
+    const id = uuidv4();
+
+    try {
+        const insertion = await pool.query('INSERT INTO todos (id, user_email, title, progress, date) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [id, user_email, title, progress, date]);
+        console.log(insertion.rows);
+        res.json(insertion.rows);
+    } 
+    catch (error) {
+        console.error(error);
     }
 });
 
