e1ff1f3131adb65e88e89fb65966bc7606123731
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-06-22 20:24:23 -0400

    Altered App and css
    Added styling for auth page and some logic in the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,55 @@
 import ListHeader from "./components/ListHeader";
 import ListItem from "./components/ListItem";
 import Auth from "./components/Auth";
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
-const App = ()=> {
+const App = () => {
   //states
   const [tasks, setTasks] = useState([]);
   const userEmail = "juansito@gmail.com";
 
   const authToken = false;
 
-
   //function to get data from db
   const getData = async () => {
     //Ask for the data to the backend
     await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
-    .then(response => response.json())
-    .then(data => {
-      setTasks(data);
-    })
-    .catch(error =>{
-      console.error(error);
-    });
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-  
-  useEffect(()=> getData, []); 
+  useEffect(() => {
+    if (authToken) {
+      getData();
+    }
+  }, [authToken]);
 
   console.log(tasks);
 
-  //Sort by date  
-  const sortedTasks = tasks?.sort((a,b)=> (new Date(a.date)) - (new Date(b.date)));
+  //Sort by date
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
 
   return (
     <div className="app">
-
       {!authToken && <Auth></Auth>}
       {/* Only shows if auth token exists */}
-      {authToken &&
-      <>
-        <ListHeader listName={" 👨🏻‍💻 CS tick list"} getData={getData}/>
-        {sortedTasks?.map((task) =><ListItem key={task.id} task={task} getData={getData}/>)}
-      </>}
-
+      {authToken && (
+        <>
+          <ListHeader listName={" 👨🏻‍💻 CS tick list"} getData={getData} />
+          {sortedTasks?.map((task) => (
+            <ListItem key={task.id} task={task} getData={getData} />
+          ))}
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
