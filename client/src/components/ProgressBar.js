5744bb0f2019e8ed61674044c45294022ddf3707
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-07-02 11:46:12 -0400

    Update ProgressBar.js
    Created the progress bar object

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,8 +1,19 @@
-function ProgressBar() {
-    return (
-      <div>
-      </div>
-    );
-  }
-  
-  export default ProgressBar;
+function ProgressBar({progress}) {
+  const colors = [
+    'teal',
+    'rgb(120,120,120)'
+  ]
+
+  const randomColor = colors[Math.floor(Math.random() % colors.length)];
+
+  return (
+    <div className="outer-bar">
+      <div 
+      className="inner-bar"
+      style={{width: `${progress}%`, backgroundColor: randomColor}}
+      ></div>
+    </div>
+  );
+}
+
+export default ProgressBar;
